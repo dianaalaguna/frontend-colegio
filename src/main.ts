45355c0d2082908f61b3b1b8426cc052c950bdf2
Héctor Enríquez <hectoren@gmail.com>
2025-05-11 20:49:37 -0500

    Prueba para producción 7

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
